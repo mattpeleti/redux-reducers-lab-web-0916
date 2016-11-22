ddd372ea6c918be543c74aa573a5c1a80441b565
github username <github email address>
2016-11-22 12:57:14 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,16 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state=[], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return state.concat(action.payload);
+    case 'REMOVE_INGREDIENT':
+      let ingredientsarr = [...state.slice(0, action.payload), ...state.slice(action.payload+1)]
+      return ingredientsarr
+      // return state.map((ele, idx) =>{
+      //   if(idx !== action.payload){
+      //     return ele
+      //   }
+      // })
+    default:
+      return state
+  }
 }
